36a80a1b50defa4568b331a287424f3133a664a8
RushiTori <lucie.fontaine@latoile.em-lyon.com>
2025-12-16 12:29:34 +0100

    push forced
    Started working on the cards (in the html)
    
    Tried to make some variables in the css
    
    Started working on createSomethingCard function skeletons in the JS

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 // This DB (as well as 3 others) have 7 keys instead of 6, so I'm using this one.
 const DB_URL = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
+
+// Card for section "Avantages"
+function createBoonCard(info) {
+    let card = document.createElement("article");
+    card.classList.add("card");
+
+    // Wireframe
+
+    // WIP
+
+    return card;
+}
+
+// Card for section "Produits"
+function createProductCard(info) {
+    let card = document.createElement("article");
+    card.classList.add("card");
+
+    // Wireframe
+
+    // WIP
+
+    return card;
+}
+
+// Card for section "Services"
+function createServiceCard(info) {
+    let card = document.createElement("article");
+    card.classList.add("card");
+
+    // Wireframe
+
+    // WIP
+
+    return card;
+}
+
+// Card for section "Temoignages"
+function createTestimonyCard(info) {
+    let card = document.createElement("article");
+    card.classList.add("card");
+
+    // Wireframe
+
+    // WIP
+
+    return card;
+}
