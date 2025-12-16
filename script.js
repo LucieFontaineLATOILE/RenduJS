064b33021213934067197a765794d218da077b4a
RushiTori <lucie.fontaine@latoile.em-lyon.com>
2025-12-16 11:25:58 +0100

    empty JS
    empty JS with just the chosen DB's URL

---
+++ script.js
@@ -0,0 +1,2 @@
+// This DB (as well as 3 others) have 7 keys instead of 6, so I'm using this one.
+const DB_URL = "https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/patisserie.json";
